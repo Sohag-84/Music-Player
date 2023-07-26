c73cc0eacf95ad1643fbdf1785f314058845027c
Sohag <ihsohag-84@gmail.com>
2023-07-26 12:26:15 +0600

    add app colors

---
+++ lib/const/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const bgColor = Color(0xFF1F212C);
+const whiteColor = Color(0xFFFFFFFF);
+const sliderColor = Color(0xFF7E70FF);
+const buttonColor = Color(0xFF60E95D);
+const bgDarkColor = Color(0xFF070B11);
